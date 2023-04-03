2e6c536073ccc68b6c84bd367a5012967d36b8db
Paolo <jorpal_10_20@outlook.com>
2023-04-03 11:08:38 -0500

    Add replace_numbers function.

--- utils/misc.py
+++ utils/misc.py
@@ -4,6 +4,15 @@ exclude = set(string.punctuation)
 def removePunctuation(token_list):
     return [token for token in token_list if token not in exclude]
 
+def replace_numbers(token_list):
+    s = []
+    for token in token_list:
+        if is_number(token):
+            s.append("DIGITO")
+        else:
+            s.append(token)
+    return s
+
 def is_number(s):
     try:
         float(s)    
