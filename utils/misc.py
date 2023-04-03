df4b4c15d971ecd72878ff9d6711d59765bb504d
Paolo <jorpal_10_20@outlook.com>
2023-04-03 11:10:54 -0500

    Syntaxis change.

--- utils/misc.py
+++ utils/misc.py
@@ -4,16 +4,16 @@ exclude = set(string.punctuation)
 def removePunctuation(token_list):
     return [token for token in token_list if token not in exclude]
 
-def replace_numbers(token_list):
+def replaceNumbers(token_list):
     s = []
     for token in token_list:
-        if is_number(token):
+        if isNumber(token):
             s.append("DIGITO")
         else:
             s.append(token)
     return s
 
-def is_number(s):
+def isNumber(s):
     try:
         float(s)    
         return True
