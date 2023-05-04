d56ab529a2bf4064e99945108a0555f45308b3d0
Paolo <jorpal_10_20@outlook.com>
2023-05-04 15:42:10 -0500

    CNN hypermodel for manifesto project

--- CNN.py
+++ CNN.py
@@ -3,12 +3,12 @@ import tensorflow as tf
 import tensorflow_addons as tfa
 import keras_tuner as kt
 from CNN_hypermodel import ConvHyperModel, SimpleConvHyperModel
-from CNN_hypermodel70 import ConvHyperModel70, SimpleConvHyperModel70
-from CNN_hypermodel65 import ConvHyperModel65, SimpleConvHyperModel65
-from CNN_hypermodel60 import ConvHyperModel60, SimpleConvHyperModel60
-from CNN_hypermodel50 import ConvHyperModel50, SimpleConvHyperModel50
-from CNN_hypermodel40 import ConvHyperModel40, SimpleConvHyperModel40
-from CNN_hypermodel30 import ConvHyperModel30, SimpleConvHyperModel30
+# from CNN_hypermodel70 import ConvHyperModel70, SimpleConvHyperModel70
+# from CNN_hypermodel65 import ConvHyperModel65, SimpleConvHyperModel65
+# from CNN_hypermodel60 import ConvHyperModel60, SimpleConvHyperModel60
+# from CNN_hypermodel50 import ConvHyperModel50, SimpleConvHyperModel50
+# from CNN_hypermodel40 import ConvHyperModel40, SimpleConvHyperModel40
+# from CNN_hypermodel30 import ConvHyperModel30, SimpleConvHyperModel30
 from GRU_hypermodel import GRUHyperModel
 
 def instanciateHypermodel(hypermodel):
@@ -16,30 +16,30 @@ def instanciateHypermodel(hypermodel):
         return ConvHyperModel()
     elif hypermodel == "simple_cnn":
         return SimpleConvHyperModel()
-    elif hypermodel == "cnn_70":
-        return ConvHyperModel70()
-    elif hypermodel == "cnn_65":
-        return ConvHyperModel65()
-    elif hypermodel == "cnn_60":
-        return ConvHyperModel60()
-    elif hypermodel == "cnn_50":
-        return ConvHyperModel50()
-    elif hypermodel == "cnn_40":
-        return ConvHyperModel40()
-    elif hypermodel == "cnn_30":
-        return ConvHyperModel30()
-    elif hypermodel == "simple_cnn_70":
-        return SimpleConvHyperModel70()
-    elif hypermodel == "simple_cnn_65":
-        return SimpleConvHyperModel65()
-    elif hypermodel == "simple_cnn_60":
-        return SimpleConvHyperModel60()
-    elif hypermodel == "simple_cnn_50":
-        return SimpleConvHyperModel50()
-    elif hypermodel == "simple_cnn_40":
-        return SimpleConvHyperModel40()
-    elif hypermodel == "simple_cnn_30":
-        return SimpleConvHyperModel30()
+    # elif hypermodel == "cnn_70":
+    #     return ConvHyperModel70()
+    # elif hypermodel == "cnn_65":
+    #     return ConvHyperModel65()
+    # elif hypermodel == "cnn_60":
+    #     return ConvHyperModel60()
+    # elif hypermodel == "cnn_50":
+    #     return ConvHyperModel50()
+    # elif hypermodel == "cnn_40":
+    #     return ConvHyperModel40()
+    # elif hypermodel == "cnn_30":
+    #     return ConvHyperModel30()
+    # elif hypermodel == "simple_cnn_70":
+    #     return SimpleConvHyperModel70()
+    # elif hypermodel == "simple_cnn_65":
+    #     return SimpleConvHyperModel65()
+    # elif hypermodel == "simple_cnn_60":
+    #     return SimpleConvHyperModel60()
+    # elif hypermodel == "simple_cnn_50":
+    #     return SimpleConvHyperModel50()
+    # elif hypermodel == "simple_cnn_40":
+    #     return SimpleConvHyperModel40()
+    # elif hypermodel == "simple_cnn_30":
+    #     return SimpleConvHyperModel30()
     elif hypermodel == "gru":
         return GRUHyperModel()
     return
@@ -51,11 +51,11 @@ class AutoCNN:
         self.model = None
         self.hypermodel = hypermodel
 
-    def fit_and_tune(self, X, y, epochs=50, validation_split=0.2, objective=["val_f1_score_micro"], n_trials=100, refit=True, **kwargs):        
+    def fit_and_tune(self, X, y=None, epochs=50, validation_split=0.2, validation_data=None, objective=["val_categorical_accuracy"], n_trials=100, refit=True, **kwargs):        
         objective_list = []
 
-        if "val_f1_score_micro" in objective:
-            objective_list.append(kt.Objective("val_f1_score_micro", direction="max"))
+        if "val_categorical_accuracy" in objective:
+            objective_list.append(kt.Objective("val_categorical_accuracy", direction="max"))
         if "val_f1_score_macro" in objective:
             objective_list.append(kt.Objective("val_f1_score_macro", direction="max"))
         if "val_loss" in objective:
@@ -82,10 +82,10 @@ class AutoCNN:
 
 
         self.tuner = tuner
-        stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
-        tuner.search(X, y, validation_split=validation_split, callbacks=[stop_early])
+        stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, mode='min')
+        tuner.search(X, y, validation_split=validation_split, validation_data=validation_data, callbacks=[stop_early])
         
-        best_hps = self.get_best_hyperparamenters()
+        best_hps = self.get_best_hyperparameters()
 
         print("Refit with best hyperparameters")
         if not refit:
@@ -95,16 +95,16 @@ class AutoCNN:
 
         hypermodel = instanciateHypermodel(self.hypermodel)
         model = hypermodel.build(best_hps)
-        history = hypermodel.fit(best_hps, model, x=X, y=y, epochs=epochs, validation_split=validation_split, callbacks=[tensorboard_callback])   
+        history = hypermodel.fit(best_hps, model, x=X, y=y, epochs=epochs, validation_split=validation_split, validation_data=validation_data, callbacks=[tensorboard_callback, stop_early])   
 
-        # val_per_epoch = history.history[objective]
-        # if objective == "val_loss":
-        #     best_epoch = val_per_epoch.index(min(val_per_epoch)) + 1
-        # best_epoch = val_per_epoch.index(max(val_per_epoch)) + 1
-        # print("*"*10)
-        # print('Best epoch: %d' % (best_epoch,))
+        val_per_epoch = history.history[objective]
+        if objective == "val_loss":
+            best_epoch = val_per_epoch.index(min(val_per_epoch)) + 1
+        best_epoch = val_per_epoch.index(max(val_per_epoch)) + 1
+        print("*"*10)
+        print('Best epoch: '.format(best_epoch))
 
-    def get_best_hyperparamenters(self, verbose=1):
+    def get_best_hyperparameters(self, verbose=1):
         if self.tuner == None:
             print("No tuner was found, please run fit_and_tune() method first")
             return
@@ -123,35 +123,31 @@ class AutoCNN:
             if self.hypermodel == "gru":
                 print("GRU units:", best_hps.get('gru_units'))
                 
-            print("Dropout rate:", best_hps.get('dropout'))
-            print("Number of linear layers:", best_hps.get('num_layers'))
+            print("Dropout rate:", best_hps.get('dropout_rate'))
+            print("Number of hidden linear layers:", best_hps.get('num_hidden_layers'))
 
-            for i in range(best_hps.get('num_layers')):
+            for i in range(best_hps.get('num_hidden_layers')):
                 print(f"Dense_{i+1}:", best_hps.get(f"units_{i}"))
 
-            print("Activation linear layer:", best_hps.get('activation_linear_layer'))
-            print("Batch normalization:", best_hps.get('batchnorm'))
+            print("Batch normalization after hidden layer:", best_hps.get('batchnorm_after_hidden_layer'))
             print("Relu before softmax:", best_hps.get('relu_before_softmax'))
-            print("Batch size:", best_hps.get('batch_size'))
             print("Learning rate", best_hps.get('learning_rate'))
             print("*"*10)
 
         return best_hps
 
-    def fit(self, X, y, epochs, validation_split=0.0):
-        best_hps = self.get_best_hyperparamenters(0)
+    def fit(self, X, y=None, epochs=20, validation_split=0.0, validation_data=None):
+        best_hps = self.get_best_hyperparameters(0)
 
         hypermodel = instanciateHypermodel(self.hypermodel)
         model = hypermodel.build(best_hps)
-        model.fit(X, y, batch_size=best_hps.get('batch_size'), epochs=epochs, validation_split=validation_split)
+        model.fit(X, y, epochs=epochs, validation_split=validation_split, validation_data=validation_data)
         self.model = model
-        return model
+        return self.model
     
-    def predict(self, X_test, y_test):
-        best_hps = self.get_best_hyperparamenters(0)    
-
+    def predict(self, X, y=None):
         print("Predicting with test data")
-        eval_result = self.model.evaluate(X_test, y_test, batch_size=best_hps.get('batch_size'))
+        eval_result = self.model.evaluate(X, y)
         print("[test loss, test f1 score,[macro]]:", eval_result)
         return 
 
--- CNN_hypermodel.py
+++ CNN_hypermodel.py
@@ -6,59 +6,71 @@ import keras_tuner as kt
 
 class ConvHyperModel(kt.HyperModel):
     def build(self, hp):
-        input_shape = (80, 100) #change
-        inputs = tf.keras.Input(shape=input_shape)
+        input_shape = (60, 300) #change
+        prev_phrase_in = tf.keras.Input(shape=input_shape, name='prev_phrase')
+        phrase_in      = tf.keras.Input(shape=input_shape, name='phrase')
+        party_in       = tf.keras.Input(shape=(1, 78), name='party')
 
         # parameters tuning
-        hp_dropout_rate = hp.Choice('dropout', values=[0.0, 0.15, 0.25, 0.5, 0.75, 0.8])
-        hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4, 1e-5])
-        hp_filters = hp.Choice('filters', values=[10, 50, 100, 150])
-        hp_activation = hp.Choice('activation_linear_layer', values=["relu", "sigmoid"])
+        hp_number_conv_layers = hp.Int("num_conv_layers", 1, 3)
+        hp_dropout_rate = hp.Choice('dropout_rate', values=[0.25, 0.5, 0.8])
+        hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
+        hp_filters = hp.Choice('filters', values=[100, 150])
 
-        conv_layers = []
-        for i in range(hp.Int("num_conv_layers", 1, 3)):
-            conv = layers.Conv1D(filters=hp_filters, kernel_size=i+2)(inputs)
-            if hp.Boolean("batchnorm"):
-                conv = layers.BatchNormalization()(conv)
+        conv_pool_layers = []
+        # Convolutions with previous phrase
+        for i in range(hp_number_conv_layers):
+            conv = layers.Conv1D(filters=hp_filters, kernel_size=i+2)(prev_phrase_in)
+            conv = layers.BatchNormalization()(conv)
             conv = layers.Activation("relu")(conv)
             # max pooling
             conv_pool = layers.MaxPooling1D(pool_size=conv.shape[1], strides=1)(conv)
-            conv_layers.append(conv_pool)
+            conv_pool_layers.append(conv_pool)
 
-        x = layers.Concatenate()(conv_layers)
+        # Convolutions with current phrase
+        for i in range(hp_number_conv_layers):
+            conv = layers.Conv1D(filters=hp_filters, kernel_size=i+2)(phrase_in)
+            conv = layers.BatchNormalization()(conv)
+            conv = layers.Activation("relu")(conv)
+            # max pooling
+            conv_pool = layers.MaxPooling1D(pool_size=conv.shape[1], strides=1)(conv)
+            conv_pool_layers.append(conv_pool)
+
+        # Append the correspond political party
+        conv_pool_layers.append(party_in)
+
+        # Concatenation of (feature embeddings + party encodings)
+        x = layers.Concatenate()(conv_pool_layers)
         x = layers.Flatten()(x)
 
-        for i in range(hp.Int("num_layers", 0, 3)):
+        for i in range(hp.Int("num_hidden_layers", 0, 1)):
             x = layers.Dropout(rate=hp_dropout_rate)(x)
-            x = layers.Dense(units=hp.Int(f"units_{i}", min_value=32, max_value=512, step=32), 
+            x = layers.Dense(units=hp.Int(f"units_{i}", min_value=64, max_value=128, step=32), 
                              activation=None)(x)
-            if hp.Boolean("batchnorm"):
+            if hp.Boolean("batchnorm_after_hidden_layer"):
                 x = layers.BatchNormalization()(x)
-            x = layers.Activation(activation=hp_activation)(x)
+            x = layers.Activation(activation='sigmoid')(x)
 
         # Final Fully Connected
         x = layers.Dropout(rate=hp_dropout_rate)(x)
-        x = layers.Dense(5, activation=None)(x)
-        if hp.Boolean("batchnorm"):
-            x = layers.BatchNormalization()(x)
+        x = layers.Dense(7, activation=None)(x)
         if hp.Boolean("relu_before_softmax"):
             x = layers.Activation("relu")(x)
-        x = layers.Activation("softmax")(x)
 
-        model=tf.keras.Model(inputs, x)
+        model = tf.keras.Model(inputs={'prev_phrase' : prev_phrase_in, 'phrase' : phrase_in, 'party' : party_in}, outputs=x)
         model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=hp_learning_rate),
-                      loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
-                      metrics=[tfa.metrics.F1Score(num_classes=5, average='micro', name='f1_score_micro'), 
-                               tfa.metrics.F1Score(num_classes=5, average='macro', name='f1_score_macro')])
+                      loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+                      metrics=['categorical_accuracy', 
+                                tfa.metrics.F1Score(num_classes=7, average='macro', name='f1_score_macro')])
 
         return model
 
     def fit(self, hp, model, *args, **kwargs):
-        hp_batch_size = hp.Int('batch_size', min_value=8, max_value=128, step=8)
+        #hp_batch_size = hp.Int('batch_size', min_value=8, max_value=128, step=8)
 
         return model.fit(
             *args,
-            batch_size=hp_batch_size,
+            #batch_size=hp_batch_size,
             **kwargs,
         )
 
@@ -108,10 +120,10 @@ class SimpleConvHyperModel(kt.HyperModel):
         return model
 
     def fit(self, hp, model, *args, **kwargs):
-        hp_batch_size = hp.Int('batch_size', min_value=8, max_value=128, step=8)
+        #hp_batch_size = hp.Int('batch_size', min_value=8, max_value=128, step=8)
 
         return model.fit(
             *args,
-            batch_size=hp_batch_size,
+            #batch_size=hp_batch_size,
             **kwargs,
         )
