8bce33a8a6df02b89d494716443ded501a64df5b
Paolo <jorpal_10_20@outlook.com>
2023-05-22 12:33:14 -0500

    Add three new instances of bert models

--- BertClassifiers.py
+++ BertClassifiers.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras import layers
 from transformers import TFBertTokenizer, TFBertModel
 
 class BertClassifier:
@@ -9,8 +10,8 @@ class BertClassifier:
         encoder_inputs = bert_tokenizer(text_in, padding="longest", truncation=True)
         outputs = bert_model(encoder_inputs)
         x = outputs['pooler_output']
-        x = tf.keras.layers.Dropout(0.5)(x)
-        x = tf.keras.layers.Dense(7, activation=None, name='classifier')(x)
+        x = layers.Dropout(0.5)(x)
+        x = layers.Dense(7, activation=None, name='classifier')(x)
 
         model = tf.keras.Model(inputs={'text' : text_in}, outputs=x)
         return model
@@ -18,27 +19,32 @@ class BertClassifier:
 
 class BertClassifierForTwoPhrases:
     def build(self, bert_tokenizer, bert_model):
-        text_in = tf.keras.Input(shape=(), dtype=tf.string, name='text')
+        prev_text_in  = tf.keras.Input(shape=(), dtype=tf.string, name='prev_text')
+        text_in       = tf.keras.Input(shape=(), dtype=tf.string, name='text')
 
-        encoder_inputs = bert_tokenizer(text_in, padding="longest", truncation=True)
+        encoder_inputs = bert_tokenizer(prev_text_in, text_in, padding="longest", truncation=True)
         outputs = bert_model(encoder_inputs)
         x = outputs['pooler_output']
-        x = tf.keras.layers.Dropout(0.5)(x)
-        x = tf.keras.layers.Dense(7, activation=None, name='classifier')(x)
+        x = layers.Dropout(0.5)(x)
+        x = layers.Dense(7, activation=None, name='classifier')(x)
 
-        model = tf.keras.Model(inputs={'text' : text_in}, outputs=x)
+        model = tf.keras.Model(inputs={'prev_text' : prev_text_in, 'text' : text_in}, outputs=x)
         return model
     
 class BertClassifierForTwoPhrasesParty:
     def build(self, bert_tokenizer, bert_model):
-        text_in = tf.keras.Input(shape=(), dtype=tf.string, name='text')
+        prev_text_in  = tf.keras.Input(shape=(), dtype=tf.string, name='prev_text')
+        text_in       = tf.keras.Input(shape=(), dtype=tf.string, name='text')
+        party_in      = tf.keras.Input(shape=(78), name='partyname')
 
-        encoder_inputs = bert_tokenizer(text_in, padding="longest", truncation=True)
+        encoder_inputs = bert_tokenizer(prev_text_in, text_in, padding="longest", truncation=True)
         outputs = bert_model(encoder_inputs)
         x = outputs['pooler_output']
-        x = tf.keras.layers.Dropout(0.5)(x)
-        x = tf.keras.layers.Dense(7, activation=None, name='classifier')(x)
+        # concatenate bert output CLS token with encoded party (both are one-dimensional vectors)
+        x = layers.Concatenate()([x, party_in])
+        x = layers.Dropout(0.5)(x)
+        x = layers.Dense(7, activation=None, name='classifier')(x)
 
-        model = tf.keras.Model(inputs={'text' : text_in}, outputs=x)
+        model = tf.keras.Model(inputs={'prev_text' : prev_text_in, 'text' : text_in, 'partyname' : party_in}, outputs=x)
         return model
 
