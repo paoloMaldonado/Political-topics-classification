774f5a9298ea0d82ae362a67904c60fff88e8bde
Paolo <jorpal_10_20@outlook.com>
2023-05-31 13:48:48 -0500

    cleaning

--- BertClassifiers.py
+++ BertClassifiers.py
@@ -31,20 +31,20 @@ class BertClassifierForTwoPhrases:
         model = tf.keras.Model(inputs={'prev_text' : prev_text_in, 'text' : text_in}, outputs=x)
         return model
     
-class BertClassifierForTwoPhrasesParty:
-    def build(self, bert_tokenizer, bert_model):
-        prev_text_in  = tf.keras.Input(shape=(), dtype=tf.string, name='prev_text')
-        text_in       = tf.keras.Input(shape=(), dtype=tf.string, name='text')
-        party_in      = tf.keras.Input(shape=(78), name='partyname')
+# class BertClassifierForTwoPhrasesParty:
+#     def build(self, bert_tokenizer, bert_model):
+#         prev_text_in  = tf.keras.Input(shape=(), dtype=tf.string, name='prev_text')
+#         text_in       = tf.keras.Input(shape=(), dtype=tf.string, name='text')
+#         party_in      = tf.keras.Input(shape=(78), name='partyname')
 
-        encoder_inputs = bert_tokenizer(prev_text_in, text_in, padding="longest", truncation=True)
-        outputs = bert_model(encoder_inputs)
-        x = outputs['pooler_output']
-        # concatenate bert output CLS token with encoded party (both are one-dimensional vectors)
-        x = layers.Concatenate()([x, party_in])
-        x = layers.Dropout(0.5)(x)
-        x = layers.Dense(7, activation=None, name='classifier')(x)
+#         encoder_inputs = bert_tokenizer(prev_text_in, text_in, padding="longest", truncation=True)
+#         outputs = bert_model(encoder_inputs)
+#         x = outputs['pooler_output']
+#         # concatenate bert output CLS token with encoded party (both are one-dimensional vectors)
+#         x = layers.Concatenate()([x, party_in])
+#         x = layers.Dropout(0.5)(x)
+#         x = layers.Dense(7, activation=None, name='classifier')(x)
 
-        model = tf.keras.Model(inputs={'prev_text' : prev_text_in, 'text' : text_in, 'partyname' : party_in}, outputs=x)
-        return model
+#         model = tf.keras.Model(inputs={'prev_text' : prev_text_in, 'text' : text_in, 'partyname' : party_in}, outputs=x)
+#         return model
 
